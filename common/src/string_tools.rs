cb305a1bb57b2958ad318add5b7ef003337ebe50
Dmitriy Krasnyansky <k.dimonych@gmail.com>
2025-11-29 22:32:27 +0200

    fix: fixed behavior of FitLineSlicer

--- common/src/string_tools.rs
+++ common/src/string_tools.rs
@@ -13,13 +13,19 @@ impl StringSlicer for str {
 
 // Word combinator
 struct FitLineSlicer<'a> {
-    message: &'a str,
+    line_splitter: core::str::Lines<'a>,
+    active_line: &'a str,
     max_len: usize,
 }
 
 impl<'a> FitLineSlicer<'a> {
     fn new(message: &'a str, max_len: usize) -> Self {
-        Self { message, max_len }
+        let res = Self {
+            line_splitter: message.lines(),
+            active_line: &message[..0],
+            max_len,
+        };
+        res
     }
 }
 
@@ -27,20 +33,19 @@ impl<'a> Iterator for FitLineSlicer<'a> {
     type Item = &'a str;
 
     fn next(&mut self) -> Option<Self::Item> {
-        if self.message.is_empty() {
-            return None;
+        if self.active_line.is_empty() {
+            self.active_line = self.line_splitter.next()?;
         }
 
         // Trim leading and trailing whitespace
-        self.message = self.message.trim();
-        let mut line = self.message;
-        line = line.lines().next()?;
+        self.active_line = self.active_line.trim();
+        let mut line = self.active_line;
 
         let line_len = line.len_in_chars();
         if line_len <= self.max_len {
             // Fits entirely, return as is
             line = line.trim();
-            self.message = &self.message[line.len()..].trim();
+            self.active_line = &self.active_line[line.len()..].trim();
             return Some(line);
         }
 
@@ -62,7 +67,7 @@ impl<'a> Iterator for FitLineSlicer<'a> {
 
         // Trim trailing whitespace if any
         line = line[..end.0].trim();
-        self.message = self.message[line.len()..].trim();
+        self.active_line = self.active_line[line.len()..].trim();
 
         Some(line)
     }
@@ -120,13 +125,32 @@ mod tests {
         let s = "This is a test message\n that will be sliced into multiple \n\n\nlines based on the maximum length specified.";
         let slicer = s.slice_by_lines(20);
         let lines: Vec<&str> = slicer.collect();
-        assert_eq!(lines.len(), 7);
+        assert_eq!(lines.len(), 9);
         assert_eq!(lines[0], "This is a test");
         assert_eq!(lines[1], "message");
         assert_eq!(lines[2], "that will be sliced");
         assert_eq!(lines[3], "into multiple");
-        assert_eq!(lines[4], "lines based on the");
-        assert_eq!(lines[5], "maximum length");
-        assert_eq!(lines[6], "specified.");
+        assert_eq!(lines[4], "");
+        assert_eq!(lines[5], "");
+        assert_eq!(lines[6], "lines based on the");
+        assert_eq!(lines[7], "maximum length");
+        assert_eq!(lines[8], "specified.");
+    }
+
+    #[test]
+    fn test_slice_by_lines_n_th_char_followed_by_nl_must_not_generate_extra_nl() {
+        let s = "This is a test message\n that will be sliced into multiple \n\n\nlines based on the maximum length specified.";
+        let slicer = s.slice_by_lines(22);
+        let lines: Vec<&str> = slicer.collect();
+        assert_eq!(lines.len(), 8);
+        assert_eq!(lines[0], "This is a test message"); // No extra split here, as max_len is 22
+        // and \n marker is at position 23 that requires to split before it by consumind the marker.
+        assert_eq!(lines[1], "that will be sliced");
+        assert_eq!(lines[2], "into multiple");
+        assert_eq!(lines[3], "");
+        assert_eq!(lines[4], "");
+        assert_eq!(lines[5], "lines based on the");
+        assert_eq!(lines[6], "maximum length");
+        assert_eq!(lines[7], "specified.");
     }
 }
